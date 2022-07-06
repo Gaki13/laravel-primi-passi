7a1018b32974a24d491a972a9bd1700ced1754a3
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-07-06 17:16:36 +0200

    Creazione home page

---
+++ resources/views/homePage.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primi passi in Lavarel</title>
+</head>
+<body>
+    <h1>Questa non è una Home Page</h1>
+    <h3>Questa non è una nav bar</h3>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Chi Siamo</li>
+            <li>Contatti</li>
+            <li>Lavora con noi</li>
+            <li>PAPERE!!!</li>
+        </ul>
+    </nav>
+</body>
+</html>
